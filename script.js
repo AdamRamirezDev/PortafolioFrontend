681fc2e9a4c8d8e908b7648e00fa79b98f1b9ed8
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2024-08-24 12:59:20 -0600

    "Se crean modulos de JavaScript con diferentes funciones"

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
-//Modo noche.
+import { activateNightMode } from "darkTheme";
+
 const icon__night__id = document.getElementById('icon__night__id');
-const main__container__id = document.getElementById('main__container__id');
-const profile__container__id = document.getElementById('profile__container__id');
-const second__wabe__id = document.getElementById('second__wabe__id');
-const second__wabe__nigth__id = document.getElementById('second__wabe__nigth__id');
-const first__wabe__night__id = document.getElementById('first__wabe__night__id');
-const first__wabe__id = document.getElementById('first__wabe__id');
-const menu__container__id = document.getElementById('menu__container__id');
-const acercaDeMi__id = document.getElementById('acercaDeMi__id');
 
-icon__night__id.addEventListener('click', () => {
+icon__night__id.addEventListener('click',activateNightMode());
 
-    main__container__id.classList.toggle('active');
-    profile__container__id.classList.toggle('active');
-    second__wabe__id.classList.toggle('active');
-    second__wabe__nigth__id.classList.toggle('active');
-    first__wabe__id.classList.toggle('active');
-    first__wabe__night__id.classList.toggle('active');
-    menu__container__id.classList.toggle('active');
-    acercaDeMi__id.classList.toggle('active');
-
-});
-
-//SECCIOND DE SKILLS
 
 
