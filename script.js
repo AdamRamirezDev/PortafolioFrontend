ed6d952082e60497353be673182a0ffeab318290
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2024-09-20 23:45:37 -0600

    "Avanzando ando"

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import {activateNightMode} from './darkTheme.js';
+import { showSoftSkills, showTechnologies } from './skills.js'; 
 
 //DarkTheme
 const icon__night__id = document.querySelector('#icon__night__id');
@@ -8,5 +9,24 @@ icon__night__id.addEventListener('click', () => {
 });
 
 //Skills section
+const skills__container__tecnologias = document.getElementById("skills__container__tecnologias__id");
+const skills__container__softSkills = document.getElementById("skills__container__softSkills__id");
+const technologies = document.getElementById("skills__technologies__text__id");
+const softSkills = document.getElementById("skills__softSkills__text__id");
+
+
+technologies.addEventListener('click', function(){
+    console.log("SI ESTA FUNCIONANDO 1");
+    skills__container__tecnologias.classList.remove('active');
+    skills__container__softSkills.classList.add("active");
+});
+
+softSkills.addEventListener('click', function(){
+    console.log("SI ESTA FUNCIONANDO 2");
+    skills__container__softSkills.classList.remove("active");
+    skills__container__tecnologias.classList.add("active");
+});
+    
+
 
 
