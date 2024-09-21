7678e1d7441727157bb27a20a21d5a182b593d0e
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2024-09-20 19:58:52 -0600

    "Modificando la seccion de habilidades"

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 import {activateNightMode} from './darkTheme.js';
 
-
-
 //DarkTheme
 const icon__night__id = document.querySelector('#icon__night__id');
 
 icon__night__id.addEventListener('click', () => {
-    
     activateNightMode();
-
 });
 
 //Skills section
-const softSkills = document.getElementById('softSkills');
+
 
