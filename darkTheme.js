a9069a77445258e5dc818bc5d6798c11f5f300be
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-05-06 12:56:45 -0600

    Agregando el modo oscuro y dandole un poco de limpieza al codigo

--- darkTheme.js
+++ darkTheme.js
@@ -10,11 +10,19 @@ export function activateNightMode(){
     const proyectos__container__title__id = document.getElementById('proyectos__container__title__id');
     const proyectos__title__id = document.querySelectorAll('#proyectos__title__id');
     const proyectos__information__container__id = document.querySelectorAll('#proyectos__information__container__id');
-    const skills__container__id = document.getElementById('skills__container__id');
+    
+    //Seccion de Skills
+    const skills__container__tecnologias__id = document.getElementById("skills__container__tecnologias__id");
+    const skills__container__softSkills__id = document.getElementById("skills__container__softSkills__id");
+    const tecnologias__div__id = document.getElementById('tecnologias__div__id');
+
+
+
     const profile__name__id = document.getElementById('profile__name__id');
     const proyectos__container__flex = document.getElementById('proyectos__container__flex__id');
     const proyectos__informacion = document.getElementById('proyectos__informacion__id');
     const profile__greeting__id = document.getElementById('profile__greeting__id');
+
 
     main__container__id.classList.toggle('active');
     profile__container__id.classList.toggle('active');
@@ -28,6 +36,10 @@ export function activateNightMode(){
     proyectos__container__flex.classList.toggle('active');
     proyectos__informacion.classList.toggle('active');
     profile__greeting__id.classList.toggle('active');
+
+    tecnologias__div__id.classList.toggle('active');
+    skills__container__tecnologias__id.classList.toggle('active');
+    skills__container__softSkills__id.classList.toggle('active');
 
     proyectos__title__id.forEach(function(elemento){
         elemento.classList.toggle('active');
