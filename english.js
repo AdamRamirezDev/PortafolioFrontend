cd454a042c19909ebcead562bd72f61372205e50
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-06-07 11:13:52 -0600

    Se continua con la implementacion del cambio de idioma, agregando las nuevas imagenes y aañadiendo los botones

---
+++ english.js
@@ -0,0 +1,14 @@
+const translations = {
+
+    es : {
+        Hola: "Hola",
+        SoyAdamRamirez: "Soy Adam Ramirez",
+
+    },
+    en: {
+        Hola: "Hello",
+        SoyAdamRamirez: "I am Adam Ramirez",
+    }
+
+
+}
